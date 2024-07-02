5dbd1af0704e2ea974274c8b39c9c95b0bb63dc0
Vidura Chathuranga Thilakasiri <vidurachathuranga2020@gmail.com>
2024-07-02 19:16:19 +0530

    added welcome and wild card urls

--- index.js
+++ index.js
@@ -7,9 +7,17 @@ import setPFP from "./api/index.js";
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.get("/", () => {
+  console.log("welcome to Slack profile picture changer");
+});
+
 app.get("/api", async (req, res) => {
   await setPFP();
   res.send("Started changing your PFP!");
+});
+
+app.get("*", (req, res) => {
+  console.log("No URL found");
 });
 
 app.listen(PORT, () => {
