7455d7dee2d84fbcc5d046b335af5f1a9d531bcf
Vidura Chathuranga Thilakasiri <vidurachathuranga2020@gmail.com>
2024-07-02 18:43:26 +0530

    move function to api folder

--- api/index.js
+++ api/index.js
@@ -5,17 +5,18 @@ dotenv.config();
 
 const images = {
   morning:
-    "https://i.pinimg.com/236x/9d/0e/25/9d0e25417e25e6b3186a42fd74daffcc.jpg",
+    "https://i.pinimg.com/564x/00/b1/ad/00b1ad02a4d71788154f1631f93f079e.jpg",
   afternoon:
-    "https://i.pinimg.com/564x/aa/e8/17/aae81731d92bbd08bea03fcbda2a3d84.jpg",
+    "https://i.pinimg.com/564x/dc/90/9c/dc909c5c6e5e70a8dcf625477270f095.jpg",
+
   evening:
-    "https://i.pinimg.com/564x/ae/0f/b5/ae0fb5afd183d634f5c77e167d303c10.jpg",
+    "https://i.pinimg.com/564x/d1/9e/be/d19ebe0590f313cd6ade19da320151f3.jpg",
   night:
-    "https://i.pinimg.com/736x/0a/7c/45/0a7c45b6f8796525e9d0399882b57b24.jpg",
+    "https://i.pinimg.com/564x/ae/0f/b5/ae0fb5afd183d634f5c77e167d303c10.jpg",
 };
 
 async function setPFP() {
-  var hour = Math.floor(new Date().getHours());
+  var hour = Math.floor(new Date().getHours() + 5.3);
   let image;
   if (5 < hour && hour < 12) {
     image = await axios.get(images.morning, {
@@ -34,7 +35,9 @@ async function setPFP() {
       responseType: "arraybuffer",
     });
   }
-
+  image = await axios.get(image, {
+    responseType: "arraybuffer",
+  });
   const client = new WebClient();
   const slackRequest = await client.users.setPhoto({
     image: image.data,
@@ -42,3 +45,7 @@ async function setPFP() {
   });
 }
 
+export default async function (req, res) {
+  await setPFP();
+  res.send("Started changing your profile picture");
+}
