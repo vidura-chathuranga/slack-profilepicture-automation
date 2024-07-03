df005879e5339dd60004ce73d88fda7dfbc95534
Vidura Chathuranga Thilakasiri <vidurachathuranga2020@gmail.com>
2024-07-04 02:09:12 +0530

    modified index js

--- src/index.js
+++ src/index.js
@@ -1,24 +1,20 @@
 import express from "express";
-import { WebClient } from "@slack/web-api";
-
+import indexRoutes from "./routes/index.routes.js";
 import "dotenv/config";
-import setPFP from "./api/index.js";
+import cors from "cors";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(cors());
+app.use(express.json());
+
 app.use("/", (req, res, next) => {
-  console.log("welcome to Slack profile picture changer");
+  console.log(`${req.method} ===> ${req.url}`);
+  next();
 });
 
-app.use("/api", async (req, res, next) => {
-  await setPFP();
-  res.send("Started changing your PFP!");
-});
-
-app.get("*", (req, res, next) => {
-  console.log("No URL found");
-});
+app.use("/api", indexRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server and run on port ${PORT}`);
